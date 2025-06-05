4a033b96e0c1168e42bf4836844324bb6c230d31
David Hadrianus <95863744+hadrianus-dev@users.noreply.github.com>
2025-06-05 17:48:28 +0100

    infrastructure: implementing state and cities tables

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use App\Trait\HasKey;
-use App\Traits\HasSlug;
+use App\Trait\HasSlug;
 use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
 use Stancl\Tenancy\Contracts\TenantWithDatabase;
 use Stancl\Tenancy\Database\Concerns\HasDatabase;
---
+++ database/migrations/2023_04_09_195346_create_states_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('states', function (Blueprint $table) {
+            $table->id();
+            $table->string('key')->unique();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('code');
+            $table->double('population');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('states');
+    }
+};
